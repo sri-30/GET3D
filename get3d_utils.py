3bc9301d674445be3108b3e7d765002d9f956553
sri-30 <asriram830@gmail.com>
2023-12-27 12:36:28 +0000

    Optimization

---
+++ get3d_utils.py
@@ -0,0 +1,110 @@
+import torch
+import dnnlib
+from torch_utils.ops import conv2d_gradfix
+from torch_utils.ops import grid_sample_gradfix
+import copy
+
+import numpy as np
+
+def constructGenerator(
+        run_dir='.',  # Output directory.
+        training_set_kwargs={},  # Options for training set.
+        G_kwargs={},  # Options for generator network.
+        D_kwargs={},  # Options for discriminator network.
+        metrics=[],  # Metrics to evaluate during training.
+        random_seed=0,  # Global random seed.
+        num_gpus=1,  # Number of GPUs participating in the training.
+        rank=0,  # Rank of the current process in [0, num_gpus[.
+        inference_vis=False,
+        inference_to_generate_textured_mesh=False,
+        resume_pretrain=None,
+        inference_save_interpolation=False,
+        inference_compute_fid=False,
+        inference_generate_geo=False,
+        code_z=None ,
+        code_tex_z=None,
+        text_prompt="",
+        **dummy_kawargs
+):
+    print("constructing")
+    from torch_utils.ops import upfirdn2d
+    from torch_utils.ops import bias_act
+    from torch_utils.ops import filtered_lrelu
+
+    upfirdn2d._init()
+    bias_act._init()
+    filtered_lrelu._init()
+
+    device = torch.device('cuda', rank)
+    np.random.seed(random_seed * num_gpus + rank)
+    torch.manual_seed(random_seed * num_gpus + rank)
+    torch.backends.cudnn.enabled = True
+    torch.backends.cudnn.benchmark = True  # Improves training speed.
+    torch.backends.cuda.matmul.allow_tf32 = True  # Improves numerical accuracy.
+    torch.backends.cudnn.allow_tf32 = True  # Improves numerical accuracy.
+    conv2d_gradfix.enabled = True  # Improves training speed.
+    grid_sample_gradfix.enabled = True  # Avoids errors with the augmentation pipe.
+
+
+    common_kwargs = dict(
+        c_dim=0, img_resolution=training_set_kwargs['resolution'] if 'resolution' in training_set_kwargs else 1024, img_channels=3)
+    G_kwargs['device'] = device
+
+    G = dnnlib.util.construct_class_by_name(**G_kwargs, **common_kwargs).train().requires_grad_(True).to(
+        device)  # subclass of torch.nn.Module
+    # D = dnnlib.util.construct_class_by_name(**D_kwargs, **common_kwargs).train().requires_grad_(False).to(
+    #     device)  # subclass of torch.nn.Module
+    G_ema = copy.deepcopy(G).eval()  # deepcopy can make sure they are correct.
+    if resume_pretrain is not None and (rank == 0):
+        print('==> resume from pretrained path %s' % (resume_pretrain))
+        model_state_dict = torch.load(resume_pretrain, map_location=device)
+        G.load_state_dict(model_state_dict['G'], strict=True)
+        G_ema.load_state_dict(model_state_dict['G_ema'], strict=True)
+        # D.load_state_dict(model_state_dict['D'], strict=True)
+    return G_ema
+
+def eval_get3d(G_ema, grid_z, grid_tex_z, grid_c):
+    G_ema.update_w_avg()
+    camera_list = G_ema.synthesis.generate_rotate_camera_list(n_batch=grid_z[0].shape[0])
+    if grid_tex_z is None:
+        grid_tex_z = grid_z
+    for i_camera, camera in enumerate(camera_list):
+        images_list = []
+        for z, geo_z, c in zip(grid_tex_z, grid_z, grid_c):
+            print(z)
+            img, mask, sdf, deformation, v_deformed, mesh_v, mesh_f, gen_camera, img_wo_light, tex_hard_mask = G_ema.generate_3d(
+                z=z, geo_z=geo_z, c=c, noise_mode='const',
+                generate_no_light=True, truncation_psi=0.7, camera=camera)
+            rgb_img = img[:, :3]
+            save_img = rgb_img.detach()
+            images_list.append(save_img.cpu().numpy())
+    images = np.concatenate(images_list, axis=0)
+    return images
+
+def eval_get3d_tensor(G_ema, grid_z, grid_tex_z, grid_c):
+    G_ema.update_w_avg()
+    camera_list = G_ema.synthesis.generate_rotate_camera_list(n_batch=grid_z[0].shape[0])
+    camera = camera_list[4]
+    output_tensor = None
+    for i, _ in enumerate(grid_z):
+        geo_z = grid_z[i]
+        tex_z = grid_tex_z[i]
+        img, mask, sdf, deformation, v_deformed, mesh_v, mesh_f, gen_camera, img_wo_light, tex_hard_mask = G_ema.generate_3d(
+            z=tex_z, geo_z=geo_z, c=grid_c, noise_mode='const',
+            generate_no_light=True, truncation_psi=0.7, camera=camera)
+        rgb_img = img[:, :3]
+        if output_tensor is None:
+            output_tensor = torch.cat((rgb_img, ))
+        else:
+            output_tensor = torch.cat((output_tensor, rgb_img))
+    return output_tensor
+
+def eval_get3d_single(G_ema, geo_z, tex_z, grid_c):
+    G_ema.update_w_avg()
+    camera_list = G_ema.synthesis.generate_rotate_camera_list()
+    camera = camera_list[4]
+    img, mask, sdf, deformation, v_deformed, mesh_v, mesh_f, gen_camera, img_wo_light, tex_hard_mask = G_ema.generate_3d(
+        z=tex_z, geo_z=geo_z, c=grid_c, noise_mode='const',
+        generate_no_light=True, truncation_psi=0.7, camera=camera)
+    rgb_img = img[:, :3]
+    return rgb_img
---
+++ latent_optimization.py
@@ -0,0 +1,115 @@
+import torch
+import numpy as np
+import copy
+import pickle
+
+from clip_utils.clip_loss import CLIPLoss
+from get3d_utils import constructGenerator, eval_get3d_single
+
+class TransformLatent(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = torch.nn.Flatten()
+        self.linear_relu_stack = torch.nn.Sequential(
+            torch.nn.Linear(512, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 512),
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+def preprocess_rgb(array):
+    lo, hi = -1, 1
+    img = array
+    img = img.transpose(1, 3)
+    img = img.transpose(1, 2)
+    img = (img - lo) * (255 / (hi - lo))
+    img.clip(0, 255)
+    return img
+
+def train_eval(G, data_geo_z, data_tex_z, text_prompt, n_epochs=5, lmbda_1=0.0015, lmbda_2=0.0015):
+    model_geo = TransformLatent().to('cuda')
+    model_tex = TransformLatent().to('cuda')
+
+    model_geo.train()
+    model_tex.train()
+
+    learning_rate_geo = 1e-3
+    optimizer_geo = torch.optim.SGD(model_geo.parameters(), lr=learning_rate_geo)
+
+    learning_rate_tex = 1e-3
+    optimizer_tex = torch.optim.SGD(model_tex.parameters(), lr=learning_rate_tex)
+    
+    loss_fn = CLIPLoss(text_prompt)
+
+    original_latents = (data_geo_z.detach().cpu(), data_tex_z.detach().cpu())
+    edited_latents = []
+    res_loss = []
+
+    for i in range(n_epochs):
+        print(i)
+        geo_z = data_geo_z.detach()
+        tex_z = data_tex_z.detach()
+
+        g_ema = copy.deepcopy(G).eval()
+
+        # Transform latents with model
+        geo_z.requires_grad = True
+        tex_z.requires_grad = True
+
+        geo_z_edited = model_geo(geo_z).reshape(1, 512)
+        tex_z_edited = model_tex(tex_z).reshape(1, 512)
+
+        # Get output of GET3D on latents
+        c = torch.ones(1, device='cuda')
+        output = eval_get3d_single(g_ema, geo_z_edited, tex_z_edited, c)
+
+        cur_output = output.detach()
+
+        # Get CLIP Loss
+        loss = loss_fn(output[0]) # + lmbda_1 * ((geo_z_edited - geo_z) ** 2).sum() + lmbda_2 * ((tex_z_edited - tex_z) ** 2).sum()
+        
+        # Backpropagation
+        loss.backward()
+
+        res_loss.append(loss[0].item())
+
+        optimizer_geo.step()
+        optimizer_geo.zero_grad()
+
+        optimizer_tex.step()
+        optimizer_tex.zero_grad()
+
+        edited_latents.append((geo_z_edited.detach().cpu(), tex_z_edited.detach().cpu()))
+    
+    return original_latents, edited_latents, res_loss
+
+if __name__ == "__main__":
+    c = None
+    with open('test.pickle', 'rb') as f:
+        c = pickle.load(f)
+
+    G_ema = constructGenerator(**c)
+
+    torch.manual_seed(58)
+
+    z = torch.randn([1, 512], device='cuda')  # random code for geometry
+    tex_z = torch.randn([1, 512], device='cuda')  # random code for texture
+
+    original, edited, loss = train_eval(G_ema, z, tex_z, 'Sports Car', 100)
+
+    print(loss)
+
+    result = []
+
+    with torch.no_grad():
+        G_ema.eval()
+        img_original = eval_get3d_single(G_ema, original[0].to('cuda'), original[1].to('cuda'), torch.ones(1, device='cuda'))
+        img_edited = eval_get3d_single(G_ema, edited[-1][0].to('cuda'), edited[-1][1].to('cuda'), torch.ones(1, device='cuda'))
+        result.append((img_original.cpu(), img_edited.cpu()))
+    with open('output_img.pickle', 'wb') as f:
+        pickle.dump(result, f)
+    
